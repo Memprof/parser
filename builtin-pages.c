bfc91990f758d50493eecda2f8d853b64dedd45a
Baptiste LEPERS <baptiste.lepers@imag.fr>
2013-10-06 12:11:34 +0200

    Show tids that access pages

--- builtin-pages.c
+++ builtin-pages.c
@@ -22,6 +22,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 static int uid;
 static uint64_t last_page, max_hole, nb_pages, min_page = -1, max_page, size;
 static rbtree pages;
+struct tid_access {
+   int tid;
+   int accesses;
+};
+struct page {
+   int accesses;
+   rbtree tid_accesses;
+};
 void pages_init() {
    pages = rbtree_create();
 }
@@ -38,22 +46,41 @@ void pages_parse(struct s* s) {
       size = ob2->size;
    }
    void *p = (void*)(s->ibs_dc_linear & PAGE_MASK);
-   int *value = rbtree_lookup(pages, p, pointer_cmp);
+   struct page *value = rbtree_lookup(pages, p, pointer_cmp);
    if(!value) {
       value = calloc(1, sizeof(*value));
+      value->tid_accesses = rbtree_create();
       rbtree_insert(pages, p, value, pointer_cmp);
    }
-   value[0] = value[0] + 1;
+   value->accesses++;
+
+   int current_tid = get_tid(s);
+   struct tid_access *realt = rbtree_lookup(value->tid_accesses, (void*)(long)current_tid, int_cmp);
+   if(!realt) {
+      realt = calloc(1, sizeof(*realt));
+      realt->tid = current_tid;
+      rbtree_insert(value->tid_accesses, (void*)(long)current_tid, realt, int_cmp);
+   }
+   realt->accesses++;
 
    max_page = max((uint64_t)p, max_page);
    min_page = min(min_page, (uint64_t)p);
 }
 
+static int print_tid(void *k, void *v) {
+   struct tid_access *t = v;
+   printf(" %d (%d)", t->tid, t->accesses);
+   return 0;
+}
+
 static int pages_print(void *k, void *v) {
+   struct page *p = v;
    nb_pages++;
    if(last_page)
       max_hole = max(max_hole, last_page - (uint64_t)k);
-   printf("%15lx [%15lx]: %d\n", (long unsigned)k, last_page - (uint64_t)k, *(int*)v);
+   printf("%15lx [%15lx]: %d (accessed by", (long unsigned)k, last_page - (uint64_t)k, p->accesses);
+   rbtree_print(p->tid_accesses, print_tid);
+   printf(")\n");
 
    last_page = (uint64_t)k;
    return 0;
